25f3c50acfddeda3d45b548aab3f77ee6aed0892
Maciej Posłuszny <xmachiei@gmail.com>
2017-04-11 16:18:53 +0200

    implemented interval-trees

--- interval-trees/src/app.js
+++ interval-trees/src/app.js
@@ -1,2 +1,84 @@
 // jshint esversion: 6, node: true
 'use strict';
+const _ = require('lodash');
+
+function median(intervals) {
+  return intervals.reduce((acc, val) => acc + val[0] + val[1], 0) / (intervals.length * 2);
+}
+
+class IntervalTree {
+  constructor(intervals) {
+    if (intervals.length === 0) return;
+
+    // wyznacz mediane i zapamietaj w wezle
+    this.median = median(intervals);
+
+    // wyznacz lewy, med i prawy
+    let left = [];
+    let middle = [];
+    let right = [];
+    intervals.forEach((interval) => {
+      if (interval[0] < this.median && interval[1] < this.median) {
+        left.push(interval);
+      }
+      else if (interval[0] > this.median && interval[1] > this.median) {
+        right.push(interval);
+      }
+      else {
+        middle.push(interval);
+      }
+    });
+
+    // stworz dwie posortowane listy przedzialow dla middle
+    // po leweym koncu
+    middle.left = intervals.slice().sort();
+    // po prawym koncu
+    middle.right = intervals.slice().sort((a, b) => a[1] - b[1]);
+
+    this.lists = {
+      left: middle.left,
+      right: middle.right
+    };
+
+    this.left = new IntervalTree(left);
+    this.right = new IntervalTree(right);
+  }
+
+  isLeaf() {
+    return typeof this.left === 'undefined';
+  }
+
+  query(x) {
+    let result = [];
+    if (this.isLeaf()) return result;
+    if (x <= this.median) {
+      this.lists.left.forEach((interval) => {
+        if (interval[0] <= x && x <= interval[1]) {
+          result.push(interval);
+        }
+        else {
+          return;
+        }
+      });
+      return result.concat(this.left.query(x));
+    }
+    else {
+      this.lists.right.forEach((interval) => {
+        if (interval[0] <= x && x <= interval[1]) {
+          result.push(interval);
+        }
+        else {
+          return;
+        }
+      });
+      return result.concat(this.right.query(x));
+    }
+  }
+}
+
+function query(intervals, x) {
+  let tree = new IntervalTree(intervals);
+  return _.uniq(tree.query(x));
+}
+
+module.exports = { query, IntervalTree, median };
--- interval-trees/test/app-test.js
+++ interval-trees/test/app-test.js
@@ -4,10 +4,30 @@
 const expect = require('chai').expect;
 const app = require('../src/app.js');
 
+let intervals = [
+  [3,  4],
+  [2,  7],
+  [6, 10],
+  [0,  2],
+  [8, 10],
+  [1,  4],
+  [4,  8],
+  [5, 10]
+];
+
+function mapInterval(interval) {
+  return { a: interval[0], b: interval[1] };
+}
+
 describe('App', () => {
-  context('Given these inputs...', () => {
-    it('Should be able to...', () => {
-      expect(1).to.be.equal(1);
-    });
+  it('Should calculate median of intervals', () => {
+    expect(app.median([[3,  4], [2,  7],])).to.be.equal(4);
+  });
+
+  it('Should return every line segment intersecting with query point', () => {
+    let expected = [[4, 8], [2, 7], [5, 10], [6, 10]];
+    let result = app.query(intervals, 6);
+    expected.forEach((interval) =>
+    expect(result.map(mapInterval)).to.include(mapInterval(interval)));
   });
 });
