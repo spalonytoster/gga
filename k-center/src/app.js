d757198b87e8669a207ef56acc7f8effa23053bc
Maciej Posłuszny <xmachiei@gmail.com>
2017-05-24 10:31:52 +0200

    ???

--- k-center/src/app.js
+++ k-center/src/app.js
@@ -5,29 +5,33 @@ function distance(p1, p2) {
   return Math.sqrt(Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2));
 }
 
-function furthestPoint(points, S, distances) {
+function furthestPoint(points, sj, distances) {
   let max = {
     distance: NaN,
     index: NaN
   };
 
-  S.forEach((s) => {
-    max = {
-      distance: _.first(distances),
-      index: 0
-    };
+  // S.forEach((s) => {
+  max = {
+    distance: _.first(distances),
+    index: 0
+  };
 
-    for (let i = 1; i < points.length; i++) {
-      if (points[i] === s) {
-        continue;
-      }
-      let dist = distances[i];
-      if (dist > max.distance) {
-        max.distance = dist;
-        max.index = i;
-      }
+  for (let i = 1; i < points.length; i++) {
+    if (points[i] === sj) {
+      continue;
     }
-  });
+    let dist = distances[i];
+    if (dist > max.distance) {
+      max.distance = dist;
+      max.index = i;
+    }
+    let sjDist = distance(points[i], sj);
+    if (sjDist > dist) {
+      distances[i] = sjDist;
+    }
+  }
+  // });
 
   let result = points[max.index];
   return result;
@@ -50,7 +54,7 @@ function kcenter(points, k) {
 
   for (let j = 1; j < k; j++) {
     let distances = calculateDistances(points, s1);
-    let sj = furthestPoint(points, S, distances);
+    let sj = furthestPoint(points, _.last(S), distances);
     S.push(sj);
   }
 
--- k-center/test/app-test.js
+++ k-center/test/app-test.js
@@ -8,11 +8,12 @@ const points = require('./points.json');
 
 describe('k-center function', () => {
   context('given k = 2', () => {
-    let k = 2;
+    let k = 3;
 
     it('Should return 2 valid centers', () => {
       let expected = [[1, 1], [3, 3]];
       let result = app.kcenter(points, k);
+      console.log('result:', result);
 
       expected.forEach((point) => assert.includeDeepMembers(result, [point]));
     });
