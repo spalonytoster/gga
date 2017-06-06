a72070e21b3eecc0f07eceffe72d1f31e540344c
Maciej Posłuszny <xmachiei@gmail.com>
2017-06-06 20:41:22 +0200

    rozkopane

--- minimum-cut-problem/src/app.js
+++ minimum-cut-problem/src/app.js
@@ -1,7 +1,53 @@
 'use strict';
+const _ = require('lodash');
 
-function fun() {
+// function random(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
 
+// graph is a collection of verges - every verge knows which whom it is connected with
+function randomMinCut(graph) {
+  let iteration = 1;
+  let size;
+
+  while ((size = _.size(graph)) > 2) {
+    console.log(`----------------- iteration ${iteration} -----------------`);
+    console.log('all verges');
+    console.log(Object.keys(graph));
+    let xKey = _.sample(Object.keys(graph));
+    let yKey = _.sample(graph[xKey]);
+    console.log(xKey);
+    console.log(graph[xKey]);
+    console.log(yKey);
+    console.log(graph[yKey]);
+
+    let X = _.reject(graph[xKey], (verge) => verge === yKey);
+    let Y = _.reject(graph[yKey], (verge) => verge === xKey);
+    let newVerge = _.uniq(_.concat(X, Y));
+
+    delete graph[xKey];
+    delete graph[yKey];
+
+    let key = 'X' + iteration++;
+    graph[key] = newVerge;
+
+    _.forEach(graph, (vergeObject, vergeKey) => {
+      vergeObject.forEach((verge, i, arr) => {
+        if (verge === xKey || verge === yKey) {
+          arr[i] = key;
+        }
+      });
+    });
+
+    console.log('new verge:');
+    console.log(newVerge);
+
+    console.log(graph);
+    if (iteration === ) {
+      break;
+    }
+  }
+  return graph;
 }
 
-module.exports = { fun };
+module.exports = { randomMinCut };
--- minimum-cut-problem/test/app-test.js
+++ minimum-cut-problem/test/app-test.js
@@ -5,6 +5,20 @@ const app = require('../src/app.js');
 
 describe('Karger\'s algorithm', () => {
   it('Should be able to find minimum cut in graph', () => {
-    expect(1).to.be.equal(1);
+    const graph = require('./example-graph.json');
+    const expected = [
+      ['F', 'H'],
+      ['F', 'D'],
+      ['F', 'G'],
+      ['H', 'D'],
+      ['H', 'E']
+    ];
+    const result = app.randomMinCut(graph);
+    console.log('expected:');
+    console.log(expected);
+
+    console.log('result:');
+    console.log(result);
+    // expect(result).to.be.deep.equal(expected);
   });
 });
