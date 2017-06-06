d36cec8d0268874f31294604d01e55a7beb3d5cf
Maciej Posłuszny <xmachiei@gmail.com>
2017-06-06 18:06:23 +0200

    created project for minimum cut problem

---
+++ minimum-cut-problem/src/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function fun() {
+
+}
+
+module.exports = { fun };
---
+++ minimum-cut-problem/test/app-test.js
@@ -0,0 +1,10 @@
+/* global it, describe */
+'use strict';
+const expect = require('chai').expect;
+const app = require('../src/app.js');
+
+describe('Karger\'s algorithm', () => {
+  it('Should be able to find minimum cut in graph', () => {
+    expect(1).to.be.equal(1);
+  });
+});
